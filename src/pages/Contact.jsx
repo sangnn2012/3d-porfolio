69f830fc8f8e8a1e8624a0bf8eb4d34b68ce0b0a
Sang <sangnn2012@gmail.com>
2024-04-09 17:57:03 +0700

    feat - try send email with emailjs

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,9 +2,9 @@ import {useState, useRef} from 'react';
 import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-    const serviceId = process.env.VITE_APP_EMAILJS_SERVICE_ID;
-    const templateId = process.env.VITE_APP_EMAILJS_TEMPLATE_ID;
-    const userId = process.env.VITE_APP_EMAILJS_PUBLIC_KEY;
+    const serviceId = import.meta.env.VITE_APP_EMAILJS_SERVICE_ID;
+    const templateId = import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID;
+    const publicKey = import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY;
 
     const formRef = useRef(null)
     const [form, setForm] = useState({ name: '', email: '', message: ''})
@@ -17,8 +17,45 @@ const Contact = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         setIsLoading(true);
+        console.log(`🚀 ~ Contact ~ form:`, form)
+        console.log(`🚀 ~ Contact ~ publicKey:`, publicKey)
+        emailjs.init({
+            publicKey,
+            // Do not allow headless browsers
+            blockHeadless: true,
+            blockList: {
+              // Block the suspended emails
+              list: ['foo@emailjs.com', 'bar@emailjs.com'],
+              // The variable contains the email address
+              watchVariable: 'userEmail',
+            },
+            limitRate: {
+              // Set the limit rate for the application
+              id: 'app',
+              // Allow 1 request per 10s
+              throttle: 10000,
+            },
+          });
 
-        // emailjs.sendForm( templateId)
+        emailjs.send(
+            serviceId,
+            templateId,
+            {
+                from_name: form.name,
+                to_name: "Sang Nguyen",
+                from_email: form.email,
+                to_email: "sang.nguyen.chainstack@gmail.com",
+                message: form.message,
+            },
+        ).then((result) => {
+            console.log(`🚀 ~ ).then ~ result:`, result)
+            setIsLoading(false);
+            // setForm({ name: '', email: '', message: ''});
+            formRef.current.reset();
+            alert('Message sent successfully');
+        }).catch((error) => {
+            console.log(`🚀 ~ ).then ~ error:`, error)
+        })
     }
     const handleFocus = () => {}
     const handleBlur = () => {}
@@ -28,7 +65,10 @@ const Contact = () => {
             <div className="flex-1 min-w=[50%] flex flex-col">
                 <h1 className="head-text">Get in Touch</h1>
 
-                <form className="w-full flex flex-col gap-7 mt-14">
+                <form
+                    ref={formRef}
+                    onSubmit={handleSubmit} 
+                    className="w-full flex flex-col gap-7 mt-14">
                     <label className="text-black-500 font-semibold">
                         Name
                         <input 
@@ -37,7 +77,7 @@ const Contact = () => {
                             className="input" 
                             placeholder="Input your name"
                             required
-                            value={form.name}
+                            value={form.name} 
                             onChange={handleChange}
                             onFocus={handleFocus}
                             onBlur={handleBlur}
